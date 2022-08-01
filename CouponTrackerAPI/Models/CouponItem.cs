5b51609048906d22fe0a5a1eca190baa4155d659
Katherine Hambley <superdesigngirl@mac.com>
2022-08-01 13:54:50 -0500

    Add project files.

---
+++ CouponTrackerAPI/Models/CouponItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CouponTrackerAPI.Models
+{
+    public class CouponItem
+    {
+        public int Id { get; set; }
+        public string CouponType { get; set; }
+        public string Category { get; set; }
+        public string StoreName { get; set; }
+        public string MfrName { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal SavingsAmount { get; set; }
+        public int SavingsQty { get; set; }
+        public string SavingsDescription { get; set; }
+        public string ImageFilename { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+    }
+}
